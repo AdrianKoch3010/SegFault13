b6d74fb6ac5d396bf7a5d3d49ad3ed860ab37fee
hussain2603 <56412306+hussain2603@users.noreply.github.com>
2020-01-29 10:10:41 +0000

    Add files via upload

---
+++ testbench_maths.m
@@ -0,0 +1,61 @@
+%maths coursework testbenches
+
+%testing with first order odes
+% -------test1--------
+dy/dx = 6x;
+y(2) = 8;
+xfinal = 15;
+h = 0.5; %step size
+% -------test2--------
+dy/dx = -5y
+y(0) = 4
+xfinal = 5
+h = 0.1
+% -------test3--------
+cos(x)y' + sin(x)y = 2cos^3(x)sin(x) - 1 
+y(pi/4) = 3sqrt(2)
+xfinal = pi/2
+h = pi/4
+% -------test4--------
+xy' + 2y = x^2 - x + 1
+y(1) = 1/2
+xfinal = 2
+h = 0.001
+% -------test5--------
+2xy^2 + 4 = 2y'(3 - x^2y)
+y(-1) = 8
+xfinal = 2
+h = 0.1
+% -------test6--------
+3(y^3)e^(3xy) - 1 +(2ye^(3xy) + 3x(y^2)e^(3xy))y' = 0
+y(0) = 1
+xfinal = 3
+h = 0.01
+% -------test7--------
+y' + y/x - sqrt(y) = 0
+y(1) = 0
+xfinal = 3
+h = 0.01
+% -------test8--------
+y' + 2y = 2 - e^(-4x) 
+y(0) = 1
+xfinal = 0.5
+h = 0.1
+% -------test9--------
+y' + 2y = 2 - e^(-4x) 
+y(0) = 1
+xfinal = 5
+h = 0.001
+% -------test10--------
+y' - y = -1/2(e^x/2)sin(5x) + 5(e^x/2)cos(5x)
+y(0) = 0
+xfinal = 5
+h = 0.001
+% -------test11--------
+y' - y = -1/2(e^x/2)sin(5x) + 5(e^x/2)cos(5x)
+y(0) = 0
+xfinal = 2
+h = 0.05
+
+
+%for more tests we can use the problem sheets put up on blackboard 
