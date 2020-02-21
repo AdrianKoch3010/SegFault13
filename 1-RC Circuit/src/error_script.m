a07bb5be4e817855fa40dbcfe25f45ec206ee2c7
Maëlle Guerre <maelle.guerre18@imperial.ac.uk>
2020-02-21 09:47:18 +0000

    error script

--- 1-RC Circuit/src/error_script.m
+++ 1-RC Circuit/src/error_script.m
@@ -3,31 +3,22 @@
 % the error is then plotted for each method.
 
 
-%Values for the current system
-R = 0.5;
-L = 0.0015;
-i0 = 0;
 
  %stop here
 h = 0.000001;
 tf = 1000*h;%step size
-tau = 120*10^-6;
+tau = 500*10^-6;
 
 %ODE for input into numerical methods - corresponds to the RL circuit
-func=@(t,i, Vin) (Vin(t) - R*i)/L;
+ODE =@;
 
-Vin =@(t) 4*cos((2*pi*t)/tau); %input of the system
+Vin =@; %input of the system
 
-%exact solution of the system
-c = -(4*(tau^2)*R)/(4*(pi^2)*(L^2) + (tau^2)*(R^2)); %Value of c for the given initial conditions
-exacti =@(t) c*exp(-(R*t)/L)+(8*pi*L*tau*sin((2*pi*t)/tau))/(4*(pi^2)*(L^2)+(tau^2)*(R^2)) + (4*(tau^2)*R*cos((2*pi*t)/tau))/(4*(pi^2)*(L^2)+(tau^2)*(R^2));
-exactout =@(t, Vin, exacti) Vin(t) - R*arrayfun(exacti, t); %Exact system response
+%numerical analysis values 
+[t1,he] = RK2(func,h, tf,t,Vin);
 
-%carry out the numerical analysis of the system
-[t1,he] = RK2(func,h, tf,Vin,R,h);
-function [x_values, y_values] = RK2(ODE, step_size, final_val, xi, yi)
-
-
+%call IDE solve to get the exact value of the function
+[t1, heun] = ode45(ODE, [t0, tf], 500);
 % Plot the error of each of the numerical methods
 %error = exact solution - numerical solution
 
