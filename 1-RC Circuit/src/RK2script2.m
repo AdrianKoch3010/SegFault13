10daced3f33d86f95fb9072807d3a5c46eb40373
hussain2603 <56412306+hussain2603@users.noreply.github.com>
2020-03-06 11:25:19 +0000

    Delete RK2script2.m

--- 1-RC Circuit/src/RK2script2.m
+++
@@ -1,6 +0,0 @@
-F = {@(x, y) (2.5-y)*10000; @(x,y) ((2.5)*exp(-x^2/100)-y)*10000};
-ODE = F(1);
-
-figure; hold on;
-[out_x1, out_y1] = RK2N(ODE, 0.001, 0.005, 0, 5, 0);
-%a1 = plot(out_x1, out_y1);
