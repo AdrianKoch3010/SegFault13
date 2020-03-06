358e5db3990ded886525183bd46d2828acffe954
hussain2603 <56412306+hussain2603@users.noreply.github.com>
2020-03-06 11:26:49 +0000

    Update RK2N.m

--- 1-RC Circuit/src/RK2N.m
+++ 1-RC Circuit/src/RK2N.m
@@ -7,10 +7,8 @@ function [x_values, y_values] = RK2N(ODE, step_size, final_val, xi, yi, RKMETHOD
     % @param xi The initial x-value
     % @param yi The initial y-value
     
-
     % Calculate the number of steps
     N = round((final_val - xi) / step_size);
-
     % Initialise output arrays
     x_values = zeros(1, N);
     y_values = zeros(1, N);
@@ -27,7 +25,6 @@ function [x_values, y_values] = RK2N(ODE, step_size, final_val, xi, yi, RKMETHOD
     % a = 0;
     % For Ralston
     %a = 1/3;
-    
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     b = 1-a;
@@ -35,14 +32,11 @@ function [x_values, y_values] = RK2N(ODE, step_size, final_val, xi, yi, RKMETHOD
     q = p;
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
     % Using the formula y_(i+1) = y(i) + h*g(x_i, y_i, h)
     % Where g = a*k_1 + b*k_2
     % And k_1 = f(x_i, y_i)
     % And k_2 = f(x_i + p*h, y_i + q*k_1*h)
-    
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
     % Run for N - 1 iterations
     for i = 1 : N - 1
        K_one = ODE(x_values(i), y_values(i));
@@ -50,10 +44,8 @@ function [x_values, y_values] = RK2N(ODE, step_size, final_val, xi, yi, RKMETHOD
        
        K_two = ODE(x_values(i)+(p*step_size), y_values(i)+q*K_one*step_size);
        y_values(i+1) = y_values(i)+step_size*(a*K_one+b*K_two);
+       
     end
-    
     % Return the output arrays
-    
-    
 end
 
