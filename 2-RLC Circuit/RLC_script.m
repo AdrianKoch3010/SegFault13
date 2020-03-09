a6917c82f48ac3aef5f38f71b90f8a92f24a5747
AdrianKoch3010 <adriankoch3010@gmail.com>
2020-03-09 00:09:34 +0000

    Added the input plot to RLC_script
    - We now need to decide what we want to show with the plots and how to
    alter the circuit

--- 2-RLC Circuit/RLC_script.m
+++ 2-RLC Circuit/RLC_script.m
@@ -19,12 +19,16 @@ V_out = R*out_z;
 
 figure; hold on;
 plot(out_x, V_out);
+input_vals = arrayfun(input, out_x);
+plot(out_x, input_vals);
 title('Test 1: Step signal')
+
+% Seconds or milliseconds????
 xlabel('Time/ms');
 ylabel('Voltage/V');
 
 %%%%%%%%%%%TEST 2: Y = Impulse and decay signal 1 %%%%%%%%%%%%%%%%%%%%%%%%
-input = @(x) 5*exp(-x^2 / 30);
+input = @(x) 5*exp(-x^2 / 0.003);
 ODE_z = @(x, y, z) ((input(x) - R*z - (1/C)*y) / L);
 
 [out_x, out_y, out_z] = RK4(ODE_y, ODE_z, 0.00003, 0.3, 0, 500 * 10^-9, 0);
@@ -34,6 +38,8 @@ V_out = R*out_z;
 
 figure; hold on;
 plot(out_x, V_out);
+input_vals = arrayfun(input, out_x);
+plot(out_x, input_vals);
 title('Test 2: Impulse decay signal')
 xlabel('Time/ms');
 ylabel('Voltage/V');
@@ -42,13 +48,15 @@ ylabel('Voltage/V');
 input = @(x) square(2*pi*(109)*x);
 ODE_z = @(x, y, z) ((input(x) - R*z - (1/C)*y) / L);
 
-[out_x, out_y, out_z] = RK4(ODE_y, ODE_z, 0.00003, 0.3, 0, 500 * 10^-9, 0);
+[out_x, out_y, out_z] = RK4(ODE_y, ODE_z, 0.00003, 0.1, 0, 500 * 10^-9, 0);
 
 % Calculate V_out as R*z
 V_out = R*out_z;
 
 figure; hold on;
 plot(out_x, V_out);
+input_vals = arrayfun(input, out_x);
+plot(out_x, input_vals);
 title('Test 3: Square wave, frequency 109 Hz')
 xlabel('Time/ms');
 ylabel('Voltage/V');
@@ -64,6 +72,8 @@ V_out = R*out_z;
 
 figure; hold on;
 plot(out_x, V_out);
+input_vals = arrayfun(input, out_x);
+plot(out_x, input_vals);
 title('Test 4: Square wave, frequency 5 Hz')
 xlabel('Time/ms');
 ylabel('Voltage/V');
@@ -72,13 +82,15 @@ ylabel('Voltage/V');
 input = @(x) square(2*pi*(500)*x);
 ODE_z = @(x, y, z) ((input(x) - R*z - (1/C)*y) / L);
 
-[out_x, out_y, out_z] = RK4(ODE_y, ODE_z, 0.00003, 0.3, 0, 500 * 10^-9, 0);
+[out_x, out_y, out_z] = RK4(ODE_y, ODE_z, 0.00003, 0.07, 0, 500 * 10^-9, 0);
 
 % Calculate V_out as R*z
 V_out = R*out_z;
 
 figure; hold on;
 plot(out_x, V_out);
+input_vals = arrayfun(input, out_x);
+plot(out_x, input_vals);
 title('Test 5: Square wave, frequency 500 Hz')
 xlabel('Time/ms');
 ylabel('Voltage/V');
@@ -87,13 +99,15 @@ ylabel('Voltage/V');
 input = @(x) sin(2*pi*(109)*x);
 ODE_z = @(x, y, z) ((input(x) - R*z - (1/C)*y) / L);
 
-[out_x, out_y, out_z] = RK4(ODE_y, ODE_z, 0.00003, 0.3, 0, 500 * 10^-9, 0);
+[out_x, out_y, out_z] = RK4(ODE_y, ODE_z, 0.00003, 0.1, 0, 500 * 10^-9, 0);
 
 % Calculate V_out as R*z
 V_out = R*out_z;
 
 figure; hold on;
 plot(out_x, V_out);
+input_vals = arrayfun(input, out_x);
+plot(out_x, input_vals);
 title('Test 6: Sine wave, frequency 109 Hz')
 xlabel('Time/ms');
 ylabel('Voltage/V');
@@ -109,7 +123,9 @@ V_out = R*out_z;
 
 figure; hold on;
 plot(out_x, V_out);
-title('Test 3: Square wave, frequency 5 Hz')
+input_vals = arrayfun(input, out_x);
+plot(out_x, input_vals);
+title('Test 7: Square wave, frequency 5 Hz')
 xlabel('Time/ms');
 ylabel('Voltage/V');
 
@@ -117,13 +133,15 @@ ylabel('Voltage/V');
 input = @(x) sin(2*pi*(500)*x);
 ODE_z = @(x, y, z) ((input(x) - R*z - (1/C)*y) / L);
 
-[out_x, out_y, out_z] = RK4(ODE_y, ODE_z, 0.00003, 0.3, 0, 500 * 10^-9, 0);
+[out_x, out_y, out_z] = RK4(ODE_y, ODE_z, 0.00003, 0.07, 0, 500 * 10^-9, 0);
 
 % Calculate V_out as R*z
 V_out = R*out_z;
 
 figure; hold on;
 plot(out_x, V_out);
-title('Test 3: Square wave, frequency 500 Hz')
+input_vals = arrayfun(input, out_x);
+plot(out_x, input_vals);
+title('Test 8: Square wave, frequency 500 Hz')
 xlabel('Time/ms');
 ylabel('Voltage/V');
