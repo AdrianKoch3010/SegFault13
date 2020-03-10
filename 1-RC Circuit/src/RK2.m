ad2a8cf1ba53cfd8f7bbd9c609b9bf674a35df81
Adrian Koch <adriankoch3010@gmail.com>
2020-03-10 11:19:54 +0000

    Removed unnecessary files
    - Cleared up the file structure

--- 1-RC Circuit/src/RK2.m
+++ 1-RC Circuit/src/RK2.m
@@ -1,4 +1,4 @@
-function [x_values, y_values] = RK2(ODE, step_size, final_val, xi, yi)
+function [x_values, y_values] = RK2N(ODE, step_size, final_val, xi, yi, RKMETHOD)
     % ODE solver
     % @param ODE The ODE to be solved in the form y' = f(x, y)
     % @param step_size aka h The distance on the x-axis between two
@@ -7,38 +7,45 @@ function [x_values, y_values] = RK2(ODE, step_size, final_val, xi, yi)
     % @param xi The initial x-value
     % @param yi The initial y-value
     
-
     % Calculate the number of steps
     N = round((final_val - xi) / step_size);
-
     % Initialise output arrays
     x_values = zeros(1, N);
     y_values = zeros(1, N);
     x_values(1) = xi;
     y_values(1) = yi;
 
-    % For euler
-    % Run for N - 1 iterations
-    %for i = 1 : N - 1
-    %    y_values(i + 1) = y_values(i) + step_size * feval(ODE, x_values(i), y_values(i));
-    %end
+    %%%%%%%% EDIT VALUE OF A FOR DIFFERENT METHODS %%%%%%%
+    % For Dynamic user input:
     
+    a = RKMETHOD;
     % For heun
+    % a = 0.5;
+    % For Midpoint
+    % a = 0;
+    % For Ralston
+    %a = 1/3;
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    b = 1-a;
+    p = 1/(2*b);
+    q = p;
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Using the formula y_(i+1) = y(i) + h*g(x_i, y_i, h)
+    % Where g = a*k_1 + b*k_2
+    % And k_1 = f(x_i, y_i)
+    % And k_2 = f(x_i + p*h, y_i + q*k_1*h)
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Run for N - 1 iterations
     for i = 1 : N - 1
-       y_predictor = y_values(i) + step_size * feval(ODE, x_values(i), y_values(i));
-       % Gradient at x (current x = x_values(i))
-       grad_1 = feval(ODE, x_values(i), y_values(i));
-       % Gradient at x + h (step size)
-       grad_2 = feval(ODE, x_values(i) + step_size, y_predictor);
+       K_one = ODE(x_values(i), y_values(i));
+       x_values(i+1) = x_values(i) + step_size;
        
-       % Average the gradient
-       y_average = (grad_1 + grad_2) * 0.5;
+       K_two = ODE(x_values(i)+(p*step_size), y_values(i)+q*K_one*step_size);
+       y_values(i+1) = y_values(i)+step_size*(a*K_one+b*K_two);
        
-       % Set new x and y
-       y_values(i + 1) = y_values(i) + (step_size * y_average);
-       x_values(i + 1) = x_values(i) + step_size;
     end
-    
+    % Return the output arrays
 end
 
