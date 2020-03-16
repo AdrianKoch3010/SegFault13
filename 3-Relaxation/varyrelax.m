f07c1170d24446a88973aebe7c93022600606cd8
Adrian Koch <adriankoch3010@gmail.com>
2020-03-16 11:16:16 +0000

    Added some grpahs for varying relax parameter

--- 3-Relaxation/varyrelax.m
+++ 3-Relaxation/varyrelax.m
@@ -1,14 +1,14 @@
-SORval = zeros(1, 100);
-xvals = zeros(1, 100);
+SORval = zeros(1, 200);
+xvals = zeros(1, 200);
 n = 1;
-for i = 1:0.01:1.99  
+for i = 1:0.005:1.995  
     % The test script for relaxation and SOR
     
     % Set the boundary function b1, b2, b3, b4
-    b1 = @(y) y;
-    b2 = @(y) y;
-    b3 = @(x) -x;
-    b4 = @(x) -x;
+    b1 = @(y) 2*cos(4*pi*y);
+    b2 = @(y) 2*cos(2*pi*y);
+    b3 = @(x) 2*cos(4*pi*x);
+    b4 = @(x) 2*cos(2*pi*x);
     
     grid_size = 100;
     required_accuracy = 0.000001;
